7cef4b33077da1a87e8fc61279d9d09ff211a550
AnPC <AnPC@computer>
2020-05-29 16:28:29 +0700

    them web 2

---
+++ Applewatch.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AppleWatch</title>
+	<link rel="stylesheet" type="text/css" href="applewatch.css">
+</head>
+<body>
+	<header>
+    <div class="container">
+    	<div class="menu">
+    		<ul>
+    			<li class="logo"><img src="./Img/img/T.jpg"></li>
+    			<li> <a href="index.html"class="current">Home</a></li>
+    			<li class="cart"><a href="cart.html" target="blank"><img src="./Img/img/cart.png"></a></li>
+    			<li><a href="Login.html" target="blank" class="signup"><span>Sign Up</span></a></li>
+                 <li><form>
+          <input type="email" placeholder="Enter ...">
+          <button type="submit" class="button_1">Search</button>
+        </form></li>
+    		</ul>
+    	</div>
+     </div>
+    </header>	
+    <nav>
+        <ul>
+        <li class="Iphone">
+        <a href="Iphone.html"target="blank">Iphone</a> 
+        </li>
+       <li class="Applewatch">
+        <a href="Applewatch.html"target="blank">Apple Watch</a> 
+       </li> 
+      <li class="MacBook"><a href="MacBook.html"target="blank">MacBook</a> 
+        </li>
+        </ul>
+    </nav>      	
+    	<div class="container1">
+    	<div class="box1">
+            <a href="./Img/img/applewatchs3.jpg">
+    		<img src="./Img/img/applewatchs3.jpg">
+    		</a>
+    		<h3>Apple Watch S3</h3>
+            <p>Price: 499$</p>
+            <div class="cart-img"><img src="./Img/img/cart.png"></div>
+    		
+    	</div>
+    	<div class="box2">
+            <a href="./Img/img/applewatchs4.jpg">
+    		<img src="./Img/img/applewatchs4.jpg" >
+    		</a>
+    		<h3>Apple Watch S4</h3>
+            <p>Price: 599$</p>
+            <div class="cart-img"><img src="./Img/img/cart.png"></div>
+    		
+    	</div>
+    	<div class="box3">
+            <a href="./Img/img/applewatchs5.jpg"></a>
+    		<img src="./Img/img/applewatchs5.jpg" >
+    		</a>
+    		<h3>Apple Watch S5 </h3>
+            <p>Price: 699$</p>
+    		<div class="cart-img"><img src="./Img/img/cart.png"></div>
+    	</div>
+    	
+    </div>
+       <section class="text">
+            <h1>Welcome to T shop</h1>
+           </section>
+        <footer>
+            <div class="footer-copyright">
+                <ul>
+                    <li>@copyright</li>
+                    <li>Taile</li>
+                    <li>GCD191010</li>
+                </ul>
+            </div>
+            <div class="footer-link"> 
+                <ul>
+                    <li><a href="https://www.facebook.com/">Facebook</a></li>
+                    <li><a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox">Gmail</a></li>
+                    <li><a href="https://www.instagram.com/">Instagram</a></li>
+                </ul>
+            </div>
+            <div class="footer-about"> 
+                <ul>
+                    <li>From Da Nang city</li>
+                    <li>Greenwich University</li>
+                    <li>GCD0705</li>
+                </ul>
+            </div>
+            </footer>
+
+   
+
+</body>
+</html>
---
+++ Macbook.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MacBook</title>
+	<link rel="stylesheet" type="text/css" href="macbook.css">
+</head>
+<body>
+	<header>
+    <div class="container">
+    	<div class="menu">
+    		<ul>
+    			<li class="logo"><img src="./Img/img/T.jpg"></li>
+    			<li> <a href="index.html"class="current">Home</a></li>
+    			<li class="cart"><a href="cart.html" target="blank"><img src="./Img/img/cart.png"></a></li>
+    			<li><a href="Login.html" target="blank" class="signup"><span>Sign Up</span></a></li>
+                 <li><form>
+          <input type="email" placeholder="Enter ...">
+          <button type="submit" class="button_1">Search</button>
+        </form></li>
+    		</ul>
+    	</div>
+     </div>
+    </header>
+     <nav>
+        <ul>
+        <li class="Iphone">
+        <a href="Iphone.html"target="blank">Iphone</a> 
+        </li>
+       <li class="Applewatch">
+        <a href="Applewatch.html"target="blank">Apple Watch</a> 
+       </li> 
+      <li class="MacBook"><a href="MacBook.html"target="blank">MacBook</a> 
+        </li>
+        </ul>
+    </nav>  		
+    	<div class="container1">
+    	<div class="box1">
+            <a href="./Img/img/macbookair133.jpg">
+    		<img src="/.Img/img/macbookair133.jpg">
+    		</a>
+    		<h3>MacBook Air 13.3"</h3>
+            <p>Price: 1999$</p>
+            <div class="cart-img"><img src="./img/cart.png"></div>
+    		
+    	</div>
+    	<div class="box2">
+            <a href="./Img/img/macbookpro13.jpg">
+    		<img src="./Img/img/macbookpro13.jpg" >
+    		</a>
+    		<h3>MacBook Pro 13"</h3>
+            <p>Price: 2499$</p>
+            <div class="cart-img"><img src="./Img/img/cart.png"></div>
+    		
+    	</div>
+    	<div class="box3">
+            <a href="./Img/img/macbookpro15.jpg">
+    		<img src="./Img/img/macbookpro15.jpg" >
+    		</a>
+    		<h3>Macbook Pro 15" </h3>
+            <p>Price: 2799$</p>
+            <div class="cart-img"><img src="./Img/img/cart.png"></div>
+    		
+    	</div>
+    </div>
+    <section class="text">
+            <h1>Welcome to T shop</h1>
+           </section>
+        <footer>
+            <div class="footer-copyright">
+                <ul>
+                    <li>@copyright</li>
+                    <li>Taile</li>
+                    <li>GCD191010</li>
+                </ul>
+            </div>
+            <div class="footer-link"> 
+                <ul>
+                    <li><a href="https://www.facebook.com/">Facebook</a></li>
+                    <li><a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox">Gmail</a></li>
+                    <li><a href="https://www.instagram.com/">Instagram</a></li>
+                </ul>
+            </div>
+            <div class="footer-about"> 
+                <ul>
+                    <li>From Da Nang city</li>
+                    <li>Greenwich University</li>
+                    <li>GCD0705</li>
+                </ul>
+            </div>
+            </footer>
+
+</body>
+</html>
+

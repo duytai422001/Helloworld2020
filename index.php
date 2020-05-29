e653485b60d0bb74883ff2a78eca54f141c5d9f8
AnPC <AnPC@computer>
2020-05-29 15:42:46 +0700

    them web 2

--- index.php
+++ index.php
@@ -1,16 +1,98 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <h1> Hello World </h1>
-        <a href="https://www.google.com/"> Google </a> 
-    </body>
+<head>
+	<title>Web</title>
+	
+        <link rel="stylesheet" type="text/css" href="style.css.php">
+</head>
+<body>
+
+	<header>
+    <div class="container">
+    	<div class="menu">
+    		<ul>
+    			<li class="logo"><img src="./img/T.jpg"></li>
+    			<li> <a href="index.html"class="current">Home</a></li>
+    			<li class="cart"><a href="cart.html" target="blank"><img src="./img/cart.png"></a></li>
+                <li><form>
+          <input type="email" placeholder="Enter ...">
+          <button type="submit" class="button_1">Search</button>
+        </form></li>
+    			<li><a href="Login.html" target="blank" class="signup"><span>Sign Up</span></a></li>
+                
+    		</ul>
+    	</div>
+    	</div>
+     </div>
+    </header>
+    <nav>
+        <ul>
+        <li class="Iphone">
+        <a href="Iphone.html"target="blank">Iphone</a> 
+        </li>
+       <li class="Applewatch">
+        <a href="Applewatch.html"target="blank">Apple Watch</a> 
+       </li> 
+      <li class="MacBook"><a href="MacBook.html"target="blank">MacBook</a> 
+        </li>
+        </ul>
+    </nav>		
+    
+
+    	
+    		<div class="container1">
+    	<div class="box1">
+    		<a href="MacBook.html"target="blank">
+    		<img src="./img/laptop.png">
+    		</a>
+    		<h3>MacBook</h3>
+    		
+    	</div>
+    	<div class="box2">
+    		<a href="Iphone.html"target="blank">
+    		<img src="./img/iphone.png" >
+    		</a>
+    		<h3>Iphone</h3>
+    		
+    	</div>
+    	<div class="box3">
+    		<a href="Applewatch.html"target="blank">
+    		<img src="./img/watch.png" >
+    		</a>
+    		<h3>Apple Watch</h3>
+    		
+    	</div>
+    </div>
+       
+        <section class="text">
+        	<h1>Welcome to T shop</h1>
+           </section>
+        <footer>
+        	<div class="footer-copyright">
+        		<ul>
+        			<li>@copyright</li>
+        			<li>Taile</li>
+        			<li>GCD191010</li>
+        		</ul>
+        	</div>
+        	<div class="footer-link"> 
+        		<ul>
+        			<li><a href="https://www.facebook.com/">Facebook</a></li>
+        			<li><a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox">Gmail</a></li>
+        			<li><a href="https://www.instagram.com/">Instagram</a></li>
+        		</ul>
+        	</div>
+        	<div class="footer-about"> 
+                <ul>
+                    <li>From Da Nang city</li>
+                    <li>Greenwich University</li>
+                    <li>GCD0705</li>
+                </ul>
+            </div>
+            </footer>
+
+   
+
+
+</body>
 </html>
